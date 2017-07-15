02abc4333bb392c0702d7b690a704d8ae6b12f1f
xiaoyann <0x0886@gmail.com>
2017-07-15 10:41:30 +0800

    update examples

--- examples/webpack/webpack.conf.js
+++ examples/webpack/webpack.conf.js
@@ -1,10 +1,8 @@
-const client = require('./deploy')
-const DeployPlugin = require('deploy-kit/plugins/webpack-plugin')
+const DeployPlugin = require('deploy-kit/plugins/sftp-webpack-plugin')
 
 const webpackConfig = {
   plugins: [
-    // webpack-plugin 会自动调用 client.upload
-    new DeployPlugin(client)
+    new DeployPlugin()
   ]
 }
 
