617ddaf36df07b51fb905deb15cb29b5e3fe725e
xiaoyann <0x0886@gmail.com>
2017-09-26 17:03:30 +0800

    cache uploaed files

--- plugins/webpack-plugin.js
+++ plugins/webpack-plugin.js
@@ -1,19 +1,36 @@
-function DeployPlugin(options) {
+const crypto = require('crypto')
+
+function md5(content) {
+  return crypto.createHash('md5').update(content).digest('hex')
 }
+
+const cacheStore = {}
+
+function DeployPlugin(options) {}
 
 DeployPlugin.prototype.apply = function(compiler) {
   const self = this
   compiler.plugin('done', function(stats) {
+    const files = []
     const assets = stats.compilation.assets
-    const files = Object.keys(assets).map(function(filename) {
+
+    Object.keys(assets).forEach(function(filename) {
       const file = assets[filename]
-      return {
-        size: file.size(),
-        filename: filename,
-        content: new Buffer(file.source(), 'utf-8'),
-        stats: {}
+      const size = file.size()
+      const source = file.source()
+      const hash = md5(source)
+      const cache = cacheStore[filename]
+      if (cache !== hash) {
+        files.push({
+          size: size,
+          filename: filename,
+          content: new Buffer(source, 'utf-8'),
+          stats: {}
+        })
+        cacheStore[filename] = hash
       }
     })
+
     self.client.exec(files)
   })
 }
